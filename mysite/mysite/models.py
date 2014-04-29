659ba3bc13769db47c77fcd3899b3a46dd2d830b
bibinette <sabineberets4@msn.com>
2014-04-29 14:15:44 +0200

    CharField wrong

--- mysite/mysite/models.py
+++ mysite/mysite/models.py
@@ -1,10 +1,10 @@
 from django.db import models
 
 class People(models.Model):
-    id = models.Charfield(max_length=10)
-    name = models.Charfield(max_length=30)
-    surname = models.Charfield(max_length=30)
-    password = models.Charfield(max_length=32)
+    identifier = models.CharField(max_length=10)
+    name = models.CharField(max_length=30)
+    surname = models.CharField(max_length=30)
+    password = models.CharField(max_length=32)
 
     def __unicode__(self):
         return self.prenom + " " + self.nom
