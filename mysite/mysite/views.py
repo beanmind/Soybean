a5cf90b0daff08dbb78a86f138b563d18eed9df3
bibinette <sabineberets4@msn.com>
2014-07-10 21:33:38 +0200

    sessions check indentification

--- mysite/mysite/views.py
+++ mysite/mysite/views.py
@@ -4,18 +4,32 @@ from forms import LoginForm
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from forms import ProfileForm
+from mysite.models import people
 
 
 def welcome(request):
-    return render(request, 'welcome.html')
-
+    if 'logged_user_id' in request.session:
+        print logged_user_id
+        logged_user_id = request.session['logger_user_id']
+        logged_user = people.objects.get(id = logged_user_id)
+        print logged_user
+        return render(request, 'welcome.html',{'logged_user':logged_user})
+    else:
+        return render(request, 'login.html', {'ban':form})
+        
 def login(request):
     if request.method=="POST":
         form = LoginForm(request.POST)
         if form.is_valid():
-            return render(request, 'welcome.html')
-        else:
-            return render(request, 'login.html', {'ban':form})
+            user_email = form.cleaned_data['email1']
+            print user_email
+            logged_user = people.objects.get(email=user_email)
+            print logged_user
+            request.session['logged_user_id'] = logged_user.id
+            print logged_user.id
+            print logged_user.name
+            
+            return render(request,'welcome.html', {'logged_user.name':logged_user.name})        
     else:
         form = LoginForm()
         return render(request, 'login.html', {'ban':form})
